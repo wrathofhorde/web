2389b56d84857aece8e3e2dc8841eec83463ff96
wrathofhorde <wrathofhorde@gmail.com>
2021-10-13 19:32:35 +0900

    이벤트 스크립트 추가

---
+++ event/event.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// 마우스이벤트객체 - 마우스 좌표
+window.addEventListener('load', () => {
+
+});
+
+// EX4-서로 다른 기능의 여러 버튼을 가진 화면에서 이벤트를
+window.addEventListener('load', () => {
+
+});
+
+// Ex3-이벤트 버블링 멈추기
+window.addEventListener('load', () => {
+
+});
+
+// Ex2-버블링을 이용한 사용자 이벤트 처리하기
+window.addEventListener('load', () => {
+
+});
+
+// 연습문제 1-선택된 레코드 삭제하기:event target
+window.addEventListener('load', () => {
+
+});
+
+// Ex 1-선택된 이미지 보여주기:event target
+window.addEventListener('load', () => {
+
+});
+
