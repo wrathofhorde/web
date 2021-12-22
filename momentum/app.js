183335adf164e1b1dce2475a3e7d3b63bd66eeb4
wrathofhorde <wrathofhorde@gmail.com>
2021-12-22 16:46:32 +0900

    input form

---
+++ momentum/app.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector('#input-form');
+const loginInput = loginForm.querySelector('input');
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(loginInput.value);
+  loginInput.value = "";
+});
